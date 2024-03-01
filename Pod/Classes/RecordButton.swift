3ff68076fb3368391dee0b79985685b382be89d6
idealfoxman0830 <146936647+idealfoxman0830@users.noreply.github.com>
2024-02-29 20:01:20 -0500

    Update RecordButton.swift

--- Pod/Classes/RecordButton.swift
+++ Pod/Classes/RecordButton.swift
@@ -190,14 +190,7 @@ open class RecordButton : UIButton {
         super.layoutSubviews()
     }
 
-     //override open func layoutSubviews() {
-       // circleLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        //circleLayer.position = CGPoint(x: self.bounds.midX,y: self.bounds.midY)
-        //circleBorder.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        //circleBorder.position = CGPoint(x: self.bounds.midX,y: self.bounds.midY)
-        //super.layoutSubviews()
-    //}
-    
+   
     
     @objc open func didTouchDown(){
         self.buttonState = .recording
